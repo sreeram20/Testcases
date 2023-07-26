e5a7ffbce094e50cf3ce8ba7bd42363e35929bf8
Sreeramr20 <80518862+Sreeramr20@users.noreply.github.com>
2023-07-26 16:38:26 +0530

    initial commit

---
+++ TestCases/TestCases/TestCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCasesApp.swift
+//  TestCases
+//
+//  Created by Sreeram R on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TestCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
